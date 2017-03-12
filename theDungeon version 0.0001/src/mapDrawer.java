73d30eab6626880a2de280a91fb0475575e88c2d
Logan G. Minto <logan@HOG.LOMIN.COM>
2017-03-12 10:18:56 -0400

    first commit

---
+++ theDungeon version 0.0001/src/mapDrawer.java
@@ -0,0 +1,70 @@
+
+public class mapDrawer {
+	
+	
+		
+	public static void printMap (int[][] a){
+		 
+		System.out.println("");
+	
+		for(int  i = 0; i<8; i++){
+		
+			System.out.print(a [i][0] + " ");
+		
+								}
+	
+		System.out.println("");
+	
+		for(int  i = 0; i<8; i++){
+		
+			System.out.print(a [i][1] + " ");
+		
+								}
+		System.out.println("");
+	
+		for(int i = 0; i<8;i++){
+			
+			System.out.print(a[i][2] + " ");
+							
+								}
+		
+		System.out.println("");
+		
+		for(int i = 0; i<8;i++){
+			
+			System.out.print(a[i][3] + " ");
+							
+								}
+		
+		System.out.println("");
+		
+		for(int i = 0; i<8;i++){
+			
+			System.out.print(a[i][4] + " ");
+							
+								}
+		System.out.println("");
+		
+		for(int i = 0; i<8;i++){
+			
+			System.out.print(a[i][5] + " ");
+							
+								}
+		System.out.println("");
+		
+		for(int i = 0; i<8;i++){
+			
+			System.out.print(a[i][6] + " ");
+							
+								}
+		System.out.println("");
+		
+		for(int i = 0; i<8;i++){
+			
+			System.out.print(a[i][7] + " ");
+							
+								}
+		
+	}
+
+}
